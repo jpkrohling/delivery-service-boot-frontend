f636a6e3079fe49c803a71f66f46d03ba7e84197
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-03-10 13:02:58 +0100

    Use Tracer Resolver
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- src/main/java/eu/javaland/tracing/frontend/FrontendApplication.java
+++ src/main/java/eu/javaland/tracing/frontend/FrontendApplication.java
@@ -1,12 +1,7 @@
 package eu.javaland.tracing.frontend;
 
-import com.uber.jaeger.Configuration;
-import com.uber.jaeger.samplers.ConstSampler;
-import io.opentracing.Tracer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -23,21 +18,5 @@ public class FrontendApplication {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.rootUri("http://localhost:8080/").build();
-    }
-
-    @Bean
-    public Tracer produceTracer() {
-        return new Configuration("javaland-boot-frontend")
-                .withReporter(
-                        new Configuration.ReporterConfiguration()
-                                .withLogSpans(true)
-                )
-                .withSampler(
-                        new Configuration.SamplerConfiguration()
-                                .withType(ConstSampler.TYPE)
-                                .withParam(1)
-                )
-                .getTracerBuilder()
-                .build();
     }
 }
